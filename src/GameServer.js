33eeef1cb1c7987027020e2aa5aed03a1c623495
FirstCodeReviewer <wargamer94@web.de>
2018-05-10 15:33:48 -0700

    supports server restart at specific time.

--- src/GameServer.js
+++ src/GameServer.js
@@ -43,6 +43,7 @@ function GameServer() {
     this.mainLoopBind = null;
     this.tickCounter = 0;
     this.disableSpawn = false;
+    this.nextServerRestart = Infinity;
 
     // Config
     this.config = {
@@ -68,6 +69,7 @@ function GameServer() {
         mobilePhysics: 0, // Whether or not the server uses mobile agar.io physics
         badWordFilter: 1, // Toggle whether you want the bad word filter on (0 to disable, 1 to enable) 
         serverRestart: 0, // Toggle whether you want your server to auto restart in minutes. (0 to disable)
+        serverRestartTimes: '', // Restart the server at a certain time of the day (eg: 00:00:00 - 06:00:00 - 12:00:00 - 18:00:00) [Use ' - ' to seperate more restarts by time]
 
         /** CLIENT **/
         serverMaxLB: 10, // Controls the maximum players displayed on the leaderboard.
@@ -200,6 +202,7 @@ GameServer.prototype.start = function () {
     if (this.config.serverStatsPort > 0) {
         this.startStatsServer(this.config.serverStatsPort);
     }
+    if (this.config.serverRestartTimes) this.nextServerRestart = this.getNextRestart();
 };
 
 GameServer.prototype.onHttpServerOpen = function () {
@@ -571,13 +574,28 @@ GameServer.prototype.timerLoop = function () {
     setTimeout(this.timerLoopBind, 0);
 };
 
+dateFrom24HourTime = (ref, hour24) => {
+    let date = new Date("1970-01-01T" + hour24);
+    date.setFullYear(ref.getFullYear(), ref.getMonth(), ref.getDate());
+    return date;
+};
+GameServer.prototype.getNextRestart = function () {
+    const now = new Date();
+    let restartTimes = this.config.serverRestartTimes.split(" - ");
+    restartTimes = restartTimes.map(a => dateFrom24HourTime(now, a).getTime());
+    return restartTimes.find(a => a > now.getTime()) || Infinity;
+};
+
 GameServer.prototype.mainLoop = function () {
     this.stepDateTime = Date.now();
     var tStart = process.hrtime();
     var self = this;
 
     // Restart
-    if (this.tickCounter > this.config.serverRestart) {
+    var serverRestartTime = Infinity;
+    if (this.config.serverRestartTimes) serverRestartTime = this.getNextRestart();
+    if (this.tickCounter > this.config.serverRestart || this.nextServerRestart != serverRestartTime) {
+        this.nextServerRestart = serverRestartTime;
         var QuadNode = require('./modules/QuadNode.js');
         this.httpServer = null;
         this.wsServer = null;
